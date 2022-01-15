0eb4fbac9752b8046252cddfb5d4b20e40c1ead4
voloshynromanmukol@gmail.com <voloshynromanmukol@gmail.com>
2022-01-15 20:46:44 +0200

    Update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int[] array = new int[]{1,3,5,7,9,11,13,15,17,19};
+        int[] array = new int[]{1,1,1,3,5,7,9,11,13,15,17,19};
         int min = array[0];
+        System.out.println("Min values in array:");
         for(int num : array){
             if(num<min){
                 min=num;
             }
         }
-        System.out.println("Min: " + min);
+        for (int j = 0; j< array.length; j++) {
+            if (min == array[j])
+                System.out.print(array[j] + ", ");
+        }
     }
 }
